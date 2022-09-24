e06d293291d1810ddbcccb0ce36a4d704904911f
avivavraham <avivavraham@mail.tau.ac.il>
2022-09-24 19:34:25 +0300

    10th task completed

---
+++ task10.py
@@ -0,0 +1,30 @@
+def solution(A):
+    min_integer = 1
+    d = {}
+    for item in A:
+        if item == min_integer:
+            i = min_integer + 1
+            while(d.get(i) != None):
+                i += 1
+            min_integer = i
+        d[item] = 1
+    return min_integer
+
+
+"""
+Write a function:
+
+def solution(A)
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000]. """
